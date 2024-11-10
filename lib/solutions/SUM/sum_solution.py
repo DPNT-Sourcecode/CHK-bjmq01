a4ab715da1971247093c176029b1a28ef3e079d4
sbx_user1051 <sbx_user1051@169.254.28.49>
2024-11-10 22:29:53 +0000

    Sun Nov 10 22:29:53 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,24 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(param1: int, param2: int)-> int:
+    """
+    Sum of 2 integers in the range of 0-100
+
+    Args:
+     parm1(int): first positive integer between 0-100
+     param2(int): second positive integer between 0-100
+
+     Returns:
+      int: Sum of the 2 arguments provided
+    """
+    # Validate Input types
+    if not isinstance(param1, int) or not isinstance(param2, int):
+        raise ValueError("Both inputs must be of integer type")
+    
+    # validate the range
+    if not (0 <= param1 <= 100) or not (0<= param2 <=100):
+        raise ValueError("Both inputs must be between 0-100")
+    
+    return param1 + param2
+    
+
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,22 @@
 from solutions.SUM import sum_solution
+import unittest
 
 
-class TestSum():
+class TestSum(unittest.TestCase):
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(0, 0) == 0
+        assert sum_solution.compute(100, 100) == 200
+
+    def test_type_mismatch(self):
+         """Check for type mismatch"""
+
+         invalid_inputs = [
+             (1.5, 2),
+             (1, "2"),
+             (None, 5),
+             ([], 1)
+         ]
+         for param1, param2 in invalid_inputs:
+             with self.subTest(a=a, b=)
+
