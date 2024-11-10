698f87fbc277bffefd38ae84e7dd15763634f2f1
sbx_user1051 <sbx_user1051@169.254.28.49>
2024-11-10 23:53:31 +0000

    Sun Nov 10 23:53:31 UTC 2024

--- lib/solutions/CHK/models.py
+++ lib/solutions/CHK/models.py
@@ -1,13 +1,18 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List
 
 @dataclass
 class SpecialOffer:
     quantity: int
     special_price: int
+    free_item: Optional[str] = None
 
 @dataclass
 class Item:
     sku: str
     price: int
-    special_offer: Optional[SpecialOffer] = None
+    special_offer: List[SpecialOffer] = None
+
+    def __post__init__(self):
+        if self.special_offers is None:
+            self.special_offers = []
