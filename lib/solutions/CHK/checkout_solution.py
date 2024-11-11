d86bcfa11d28e079a2a1f0d3f6edc4e3da93acdb
sbx_user1051 <sbx_user1051@169.254.28.49>
2024-11-11 02:28:44 +0000

    Mon Nov 11 02:28:44 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,15 +9,16 @@ def checkout(skus: str)-> int:
     return checkout_obj.checkout(skus=skus)
 
 
-if __name__ == '__main__':
-    checkout("STX")
-    print("expected: 45")
-    checkout("STXSTX")
-    print("expected: 90")
-    checkout("SSS")
-    print("expected: 45")
-    # checkout("EEB")
+# if __name__ == '__main__':
+#     checkout("STX")
+#     print("expected: 45")
+#     checkout("STXSTX")
+#     print("expected: 90")
+#     checkout("SSS")
+#     print("expected: 45")
+#     # checkout("EEB")
     # checkout("AAAAAAAA")
     # checkout("NNNM")
     # c
+
 
