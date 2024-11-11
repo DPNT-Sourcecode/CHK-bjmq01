e1054a55c10b954dcce4af0940360950f6c5628f
sbx_user1051 <sbx_user1051@169.254.28.49>
2024-11-11 01:17:15 +0000

    Mon Nov 11 01:17:15 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,15 +2,17 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-from checkout import Checkout
+from .checkout import Checkout
 
 def checkout(skus: str)-> int:
     checkout_obj = Checkout()
     return checkout_obj.checkout(skus=skus)
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-    # checkout("EEB")
-    # checkout("AAAAAAAA")
+    checkout("EEB")
+    checkout("AAAAAAAA")
+    checkout("FFF")
+
 
