49dca2b81ab531148b5d8131c763b1d78ed594e7
sbx_user1051 <sbx_user1051@169.254.28.49>
2024-11-11 01:26:15 +0000

    Mon Nov 11 01:26:15 UTC 2024

--- lib/solutions/CHK/models.py
+++ lib/solutions/CHK/models.py
@@ -6,6 +6,7 @@ class SpecialOffer:
     quantity: int
     special_price: int
     free_item: Optional[str] = None
+    enforced_free_item_count: int = 0
 
 @dataclass
 class Item:
@@ -16,4 +17,5 @@ class Item:
     def __post__init__(self):
         if self.special_offers is None:
             self.special_offers = []
+
 
